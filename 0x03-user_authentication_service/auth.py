41cecd8645b31b9a68796a68d276b3c8ffb0df9e
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-16 15:36:59 +0100

    register new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,7 +9,7 @@ import bcrypt
 from db import DB
 from user import User
 from user import Base
-
+from sqlalchemy.orm.exc import NoResultFound
 
 def _hash_password(password: str) -> bytes:
     """hashes and salts strings using bcrpt.hashpw"""
@@ -29,9 +29,10 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """the register method"""
-        if self._db.find_user_by(email=email):
-            raise ValueError("User {} already exists.".format(email))
-        else:
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+        raise ValueError("User {} already exists.".format(email))
