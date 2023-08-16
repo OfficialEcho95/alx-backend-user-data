13e1f03ad7c416e30e5d60ec9e293053da720dd4
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-16 15:30:18 +0100

    register new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,7 +6,9 @@ and hashes it
 
 
 import bcrypt
-
+from db import DB
+from user import User
+from user import Base
 
 def _hash_password(password: str) -> bytes:
     """hashes and salts strings using bcrpt.hashpw"""
@@ -14,3 +16,21 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(pswd, salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """init"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """the register method"""
+        if self._db.find_user_by(email=email):
+            raise ValueError("User {} already exists.".format(email))
+        else:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
