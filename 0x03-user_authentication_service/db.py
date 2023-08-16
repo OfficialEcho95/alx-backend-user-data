84d98430adadf708ca63710da4f612edd602c91f
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-16 15:02:54 +0100

    add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password) -> User:
         """returns a User object"""
         user_add = User(email=email, hashed_password=hashed_password)
         self._session.add(user_add)
