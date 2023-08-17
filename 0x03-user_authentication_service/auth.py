d305a1218674165894ee71417de1ea3bbc8008e5
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 03:34:00 +0100

    validate user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,3 +37,13 @@ class Auth:
             user = self._db.add_user(email, hashed_password)
             return user
         raise ValueError("User {} already exists.".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validating that a user exists and can log in"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        user_password = user.hashed_password
+        return bcrypt.checkpw(password.encode('utf-8'), user_password)
