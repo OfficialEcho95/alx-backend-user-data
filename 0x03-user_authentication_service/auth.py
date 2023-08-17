c090c9f30c9f68a6599f71ce3cdde1baf511f134
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 12:17:38 +0100

    updated return

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -65,7 +65,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str):
+    def get_user_from_session_id(self, session_id):
         """searches session id and returns the user"""
         user = None
         if session_id is None:
