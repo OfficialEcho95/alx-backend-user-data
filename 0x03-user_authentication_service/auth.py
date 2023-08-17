20ade5639ab6dec8f4302eb712d91cb27009c333
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 12:51:05 +0100

    reset user reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,12 +86,11 @@ class Auth:
         """update user reset token"""
         if user == None:
             raise ValueError
-        try:
-            user = self._db.find_user_by(email)
-            if user:
-                session_id = _generate_uuid()
-                reset_token = self._db.update_user(
-                    email, reset_token=session_id)
-                return reset_token
-        except ValueError:
-            return None
+        user = self._db.find_user_by(email)
+        if user:
+            session_id = _generate_uuid()
+            reset_token = self._db.update_user(
+                email, reset_token=session_id)
+            return reset_token
+        else:
+            raise ValueError
