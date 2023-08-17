0875c33ef3c16701f596ebe88b30b7f32b27d104
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 03:39:17 +0100

    uuid generator

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,7 +22,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """uuid generator"""
     return str(uuid.uuid4())
 
