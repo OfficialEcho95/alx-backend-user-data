8330dac43440ec7110025d1efaa932357132885d
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 04:27:58 +0100

    session route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 the flask app
 """
-from flask import Flask, request, jsonify
+from flask import Flask, abort, request, jsonify
 from auth import Auth
 
 
@@ -27,6 +27,20 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email=email, password=password):
+        abort(401)
+
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
 
 
 if __name__ == "__main__":
