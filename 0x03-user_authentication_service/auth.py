de6410f4fa6b63bb3a2dd1a59072f846ae9c5db4
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 12:16:12 +0100

    updated return

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,9 +72,9 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user
         except NoResultFound:
             return None
+            return user
 
     def destroy_session(self, user_id: int) -> None:
         """destroys the user's session"""
