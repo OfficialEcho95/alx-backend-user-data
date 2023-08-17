ca242d89ed71d74148e80eba6934d925bf3123cd
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 03:05:03 +0100

    user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -23,7 +23,7 @@ def users() -> str:
     email = request.form.get('email')
     password = request.form.get('password')
     try:
-        AUTH.register_user(email, password):
+        AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
