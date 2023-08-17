f31cce3d49a0dcb7b4bbe9b5d6814a74a83054a1
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-17 03:04:05 +0100

    user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,19 +18,15 @@ def welcome():
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register_users() -> str:
+def users() -> str:
     """the endpoint to register users"""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
-        if not AUTH.register_user(email, password):
-            return jsonify({"email": email, "message": "user created"}), 201
-        else:
-            return jsonify({"message": "email already registered"}), 400
-
-    except Exception as e:
-        return jsonify({"message": str(e)}), 400
-
+        AUTH.register_user(email, password):
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
